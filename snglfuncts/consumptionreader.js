ede6a17e46f0328b310039f024e37f6537cd0c1f
AdrianTayag <vincettayag@gmail.com>
2019-08-29 14:19:17 +0800

    bugfixing

--- snglfuncts/consumptionreader.js
+++ snglfuncts/consumptionreader.js
@@ -1,22 +1,20 @@
 const ina219 = require('ina219')
 ina219.init(0x45)
 ina219.calibrate32V1A(function(){ console.log("RPi Tracker calibrated")})
-var i = 1
 var x, y
-while (i == 1) {
-  ina219.getBusVoltage_V(respondV)
-  setTimeout(power, 1000)
-}
 
-const power() {
+function measure() {
+  ina219.getBusVoltage_V(respondV)
   ina219.getCurrent_mA(respondA)
 }
 
-async function respondV (voltage) {
+function respondV (voltage) {
   x = voltage
 }
 
-async function respondA (current) {
+function respondA (current) {
   y = current * x //milliwatts
   console.log(y)
 }
+
+setInterval(measure, 1000)
