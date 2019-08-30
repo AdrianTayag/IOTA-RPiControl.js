7922b5a48573f619846ebcbd8fa504951d9e584f
AdrianTayag <vincettayag@gmail.com>
2019-08-30 21:30:25 +0800

    bugfixing

--- snglfuncts/consumptionreader.js
+++ snglfuncts/consumptionreader.js
@@ -2,7 +2,7 @@ const ina219 = require('ina219')
 ina219.init(0x45)
 ina219.calibrate32V1A(function(){ console.log("RPi Tracker calibrated")})
 const createCsvWriter = require('csv-writer').createObjectCsvWriter
-var path = './trial' + (process.argv[2]) + '.csv'
+var path = './trial' + (process.argv[3]) + '.csv'
 const csvWriter = createCsvWriter({ path: path })
 const power = ["Measured"]
 var x, y, z
@@ -10,6 +10,14 @@ var x, y, z
 function measure() {
   ina219.getBusVoltage_V(respondV)
   ina219.getCurrent_mA(respondA)
+  z = z+1
+  if (z == 60) {
+    clearInterval(record)
+    csvWriter.writeRecords(records)       // returns a promise
+      .then(() => {
+        console.log('Done saving.')
+      })
+  }
 }
 
 function respondV (voltage) {
@@ -20,14 +28,6 @@ function respondA (current) {
   y = current * x //milliwatts
   power.push(y)
   console.log(y)
-  if (z == 60) {
-    clearInterval(record)
-    csvWriter.writeRecords(records)       // returns a promise
-      .then(() => {
-        console.log('Done saving.')
-      })
-  }
-  z = z+1
 }
 
 var record = setInterval(measure, 500)
